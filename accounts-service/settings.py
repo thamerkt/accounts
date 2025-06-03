cbad1821ca93b33956a9bcafe0ee2e9f6dbd3c21
thamerkt <kthirithamer1@gmail.com>
2025-06-03 14:35:38 +0100

    latest

--- accounts-service/settings.py
+++ accounts-service/settings.py
@@ -38,12 +38,13 @@ SESSION_COOKIE_AGE = 3600  # 1 hour
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379/1'),
+        'LOCATION': 'redis://red-d0usms6mcj7s73acbs30:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     }
 }
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
